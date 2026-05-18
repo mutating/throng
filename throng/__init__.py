4c6c3b482eed21c6b164b51142ff6654e5a52a98
Евгений Блинов <pomponchik@Mac-mini-Evgenij.local>
2026-05-18 15:10:50 +0300

    Update import paths to use throng namespace

--- throng/__init__.py
+++ throng/__init__.py
@@ -1,2 +1,2 @@
-from pupupu.isolates.abstract import AbstractIsolate as AbstractIsolate
-from pupupu.isolates.directory import AbstractIsolate as AbstractIsolate
+from throng.isolates.abstract import AbstractIsolate as AbstractIsolate
+from throng.isolates.directory import AbstractIsolate as AbstractIsolate

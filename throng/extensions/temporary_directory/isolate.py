980b3018ab04c9ed38dc2bd9f25149ee30ca8efa
Евгений Блинов <pomponchik@Mac-mini-Evgenij.local>
2026-09-22 14:32:25 +0300

    Add informative messages to DirectoryDoesNotExistError

--- throng/extensions/temporary_directory/isolate.py
+++ throng/extensions/temporary_directory/isolate.py
@@ -23,25 +23,25 @@ class TemporaryDirectoryIsolate(AbstractIsolate):
     def run(self, command: str, token: AbstractToken = DefaultToken()) -> SubprocessResult:  # noqa: B008
         with self.lock:
             if self.used:
-                raise DirectoryDoesNotExistError()
+                raise DirectoryDoesNotExistError('You cannot reuse a destroyed isolate.')
             return run(command, token=token, catch_output=True, directory=self.path)
 
     def read(self) -> bytes:
         with self.lock:
             if self.used:
-                raise DirectoryDoesNotExistError()
+                raise DirectoryDoesNotExistError('You cannot re-read the state of a destroyed isolate.')
             return read_directory(self.path)
 
     def set_state(self, state: bytes) -> None:
         with self.lock:
             if self.used:
-                raise DirectoryDoesNotExistError()
+                raise DirectoryDoesNotExistError('You cannot reuse a destroyed isolate.')
             with tarfile.open(fileobj=BytesIO(state), mode="r:*") as tar:
                 tar.extractall(path=self.path)
 
     def kill(self) -> None:
         with self.lock:
             if self.used:
-                raise DirectoryDoesNotExistError()
+                raise DirectoryDoesNotExistError('You cannot kill again a destroyed isolate.')
             self.directory.cleanup()
             self.used = True

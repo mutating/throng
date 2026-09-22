5ad8692549987c91a205c55608986ff8f096817c
Евгений Блинов <pomponchik@Mac-mini-Evgenij.local>
2026-09-22 13:37:25 +0300

    Add temporary_directory plugin to extensions

--- throng/__init__.py
+++ throng/__init__.py
@@ -1,4 +1,6 @@
 from throng.abstracts.abstract_manager import AbstractManager as AbstractManager
-from throng.extensions.local.manager import LocalManager as LocalManager
-from throng.extensions.plugins import local as local
 from throng.slots import throng as throng
+from throng.extensions.plugins import (
+    local as local,
+    temporary_directory as temporary_directory,
+)

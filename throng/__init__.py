272758d31e75d036ad40ee20fe3f136a3dfcbd0d
Евгений Блинов <pomponchik@Mac-mini-Evgenij.local>
2026-09-22 15:38:36 +0300

    Reorder imports in throng/__init__.py

--- throng/__init__.py
+++ throng/__init__.py
@@ -1,8 +1,10 @@
+# ruff: noqa: I001
+
 from throng.abstracts.abstract_isolate import AbstractIsolate as AbstractIsolate
 from throng.abstracts.abstract_manager import AbstractManager as AbstractManager
 from throng.abstracts.result_protocol import RunResultProtocol as RunResultProtocol
+from throng.slots import throng as throng
 from throng.extensions.plugins import (
     local as local,
     temporary_directory as temporary_directory,
 )
-from throng.slots import throng as throng

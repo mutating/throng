8e9feead0dfa04be9a94813f705ad5bcb03ef942
Евгений Блинов <pomponchik@Mac-mini-Evgenij.local>
2026-09-23 16:47:54 +0300

    Restructure result protocol and add SimpleRunResult

--- throng/abstracts/results.py
+++ throng/abstracts/results.py
@@ -1,7 +1,16 @@
 from typing import Optional, Protocol
+from dataclasses import dataclass
 
 
 class RunResultProtocol(Protocol):
+    success: bool
+    returncode: Optional[int] = None
+    stdout: Optional[str] = None
+    stderr: Optional[str] = None
+
+
+@dataclass
+class SimpleRunResult:
     success: bool
     returncode: Optional[int] = None
     stdout: Optional[str] = None
